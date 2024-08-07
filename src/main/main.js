93190849c9e61ec6259f49c121e85b08a26c5b64
Avi Aryan <avi.aryan123@gmail.com>
2024-08-07 23:17:03 +0530

    move main codes to a subdir

--- src/main/main.js
+++ src/main/main.js
@@ -21,7 +21,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
